e60d8422efb1c7aef9fd7e4560a0a6578af98917
Mottttton <mottttton.webdev@gmail.com>
2024-02-08 18:01:10 +0900

    submit task: modify routing, validations, CSS

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ApplicationRecord
+  validates :content, length: { in: 1..140,
+                                too_long: '140文字以下でTweetしてください',
+                                too_short: 'Tweet内容を入力してください' }
+
 end
